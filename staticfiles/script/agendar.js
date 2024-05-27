daee047dd8299cac21ac12471166a56796689cb7
Gustavo <r_hinchliffe1761@outlook.com>
2024-05-27 11:07:04 -0300

    delete staticfiles

--- staticfiles/script/agendar.js
+++
@@ -1,8 +0,0 @@
-const fundo_pc = document.querySelector(".mobile-version");
-const telaMobile = window.screen.width;
-
-if (telaMobile < 600) {
-    fundo_pc.classList.remove('min-h-screen');
-} else {
-    fundo_pc.classList.add('min-h-screen');
-}
